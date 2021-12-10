3d4127e371429bc7e44405a613a0225b59ab08d9
perezalexreal <perezalexreal@gmail.com>
2021-12-10 15:55:24 +0100

    logica

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 let  numbers = document.querySelector('.numbers');
 
+let container = document.querySelector('#container');
+
+let divHtml = document.createElement('div');
+
+divHtml.classList.add('col');
+
+
 for( let i = 0; i <= 100 ; i++){
-   console.log(i);
+
+    if (i % 3 === 0 && i % 5 === 0  ){
+        console.log("FizzBuzz");
+        divHtml.append('FizzBuzz');
+        container.append(divHtml);
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        divHtml.append('Fizz') ;
+        container.append(divHtml);
+
+    } else if(i % 5 === 0){
+        console.log("Buzz");
+        divHtml.append('Buzz');
+        container.append(divHtml);
+
+    } else {
+        console.log(i);
+        divHtml.append(i);
+        container.append(divHtml);
+    }
 }
 
